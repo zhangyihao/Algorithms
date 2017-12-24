2d2fd13033b7a97fc8c273f7e440c7fe0f86bbdd
zhangyihao <zyh5540@163.com>
2017-12-24 23:13:30 +0800

    commit

--- com.zhangyihao.algorithms/com/zhangyihao/algorithms/sort/BubbleSort.java
+++ com.zhangyihao.algorithms/com/zhangyihao/algorithms/sort/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-	private void sort(int[] array) {
+	public void sort(int[] array) {
 		//控制总共循环次数
 		for(int i=1; i<=array.length; i++) {
 			for(int j=0; j<array.length-i; j++) {
@@ -16,7 +16,9 @@ public class BubbleSort {
 			}
 			System.out.println("第"+i+"趟排序结果："+Arrays.toString(array));
 		}
-		
+	}
+	
+	public void sort2(int[] array) {
 		for(int i=1; i<=array.length; i++) {
 			for(int j=array.length-1; j>=i; j--) {
 				if(array[j] <= array[j-1]) {
