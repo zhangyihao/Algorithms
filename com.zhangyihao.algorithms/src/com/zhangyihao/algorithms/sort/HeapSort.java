133c3ad84f13894657d856c14758e96a7cd0a99a
zhangyihao <zhangyihao@jiuqi.com.cn>
2018-01-08 15:31:55 +0800

    optimize heap sort

--- com.zhangyihao.algorithms/src/com/zhangyihao/algorithms/sort/HeapSort.java
+++ com.zhangyihao.algorithms/src/com/zhangyihao/algorithms/sort/HeapSort.java
@@ -6,49 +6,48 @@ public class HeapSort {
 
 	public int[] sort(int[] array) {
 		int length = array.length;
-		
-		while(length>1) {
+		while (length > 1) {
 			initHeap(array, length);
-			
-			System.out.println("初始化堆结果："+Arrays.toString(array));
-			
-			int temp = array[length-1];
-			array[length-1] = array[0];
+
+			System.out.println("初始化堆结果：" + Arrays.toString(array));
+
+			int temp = array[length - 1];
+			array[length - 1] = array[0];
 			array[0] = temp;
-			
-			System.out.println("排序结果："+Arrays.toString(array));
+
+			System.out.println("排序结果：" + Arrays.toString(array));
 			length--;
 		}
 		return array;
 	}
-	
+
 	private void initHeap(int[] array, int length) {
-		//从最后一个非叶子节点的父节点开始
-		for(int i = (length-1)/2; i>=0; i--) {
+		// 从最后一个非叶子结点的父结点开始
+		for (int i = (length - 2) / 2; i >= 0; i--) {
 			int parent = i;
-			int parentValue = array[parent]; //记录下parent的值
-			int maxChild = 2*parent+1; //先选取左节点为大节点
-			
-			while(maxChild < length) {
-				// 如果有右节点且右节点大于左节点，选取右节点为大节点
-				if((maxChild+1)<length && array[maxChild] < array[maxChild+1]) {
+			int parentValue = array[parent]; // 记录下parent的值
+			int maxChild = 2 * parent + 1; // 先选取左结点为大结点
+
+			while (maxChild < length) {
+				// 如果有右结点且右结点大于左结点，选取右结点为大结点
+				if ((maxChild + 1) < length && array[maxChild] < array[maxChild + 1]) {
 					maxChild++;
 				}
-				
-				//如果大节点小于父节点直接结束
-				if(array[maxChild] <= parentValue) {
+
+				// 如果大结点小于父结点直接结束
+				if (array[maxChild] <= parentValue) {
 					break;
 				}
-				
-				//将大节点的值赋值给父节点
+
+				// 将大结点的值赋值给父结点
 				array[parent] = array[maxChild];
-				//发生交换后，继续调整已大节点为根节点的树
+				// 发生交换后，继续调整已大结点为根结点的树
 				parent = maxChild;
 				maxChild = 2 * parent + 1;
 			}
-			
+
 			array[parent] = parentValue;
 		}
 	}
-	
+
 }
---
+++ com.zhangyihao.algorithms/src/com/zhangyihao/algorithms/sort/HeapSort2.java
@@ -0,0 +1,51 @@
+package com.zhangyihao.algorithms.sort;
+
+import java.util.Arrays;
+
+public class HeapSort2 {
+	private static void adjustHeat(int[] array, int parent, int length) {
+		// temp保存当前父节点
+		int parentValue = array[parent];
+		// 先选取左结点为大结点
+		int maxChild = 2 * parent + 1;
+
+		while (maxChild < length) {
+			// 如果有右结点且右结点大于左结点，选取右结点为大结点
+			if ((maxChild + 1) < length && array[maxChild] < array[maxChild + 1]) {
+				maxChild++;
+			}
+
+			// 如果大结点小于父结点直接结束
+			if (array[maxChild] <= parentValue) {
+				break;
+			}
+
+			// 将大结点的值赋值给父结点
+			array[parent] = array[maxChild];
+			// 发生交换后，继续调整以大结点为根结点的树
+			parent = maxChild;
+			maxChild = 2 * parent + 1;
+		}
+		array[parent] = parentValue;
+	}
+
+	public void sort(int[] array) {
+		// 循环建立初始堆
+		for (int i = (array.length - 2) / 2; i >= 0; i--) {
+			adjustHeat(array, i, array.length);
+		}
+
+		// 进行n-1次循环，完成排序
+		for (int length = array.length - 1; length > 0; length--) {
+			// 最后一个元素和第一元素进行交换
+			int temp = array[length];
+			array[length] = array[0];
+			array[0] = temp;
+
+			System.out.println(String.format("第 %d 趟排序结果：", array.length - length) + Arrays.toString(array));
+
+			// 将剩下数组总元素组成的堆继续调整
+			adjustHeat(array, 0, length);
+		}
+	}
+}
