c1862b78a000166495c6c8497b85134e43e9bd0e
zhangyihao <zyh5540@163.com>
2021-12-08 22:11:31 +0800

    215 数组中第 K 个大的元素

---
+++ com.zhangyihao.algorithms/src/com/zhangyihao/algorithms/leetcode/q0206/FindKthLargeNum.java
@@ -0,0 +1,26 @@
+package com.zhangyihao.algorithms.leetcode.q0206;
+
+import java.util.PriorityQueue;
+
+/**
+ * 215 数组中第 K 个大的元素
+ */
+public class FindKthLargeNum {
+
+    public int findKthLargest(int[] nums, int k) {
+        // 小顶堆，堆顶是最小元素
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int e : nums) {
+            // 每个元素都要过一遍二叉堆
+            pq.offer(e);
+            // 堆中元素多于 k 个时，删除堆顶元素
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        // pq 中剩下的是 nums 中 k 个最大元素，
+        // 堆顶是最小的那个，即第 k 个最大元素
+        return pq.peek();
+    }
+
+}
